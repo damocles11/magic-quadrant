a7bf4f3d07953fc53dbd19b104322e82594b9ab3
Tolga Oğuz <tolga.oguz@dt.net.tr>
2021-09-26 23:22:16 +0300

    Error Handled

--- src/components/Labels.tsx
+++ src/components/Labels.tsx
@@ -8,6 +8,11 @@ function Labels() {
         <div style={{position:"absolute", bottom:"-5%", left:"0%"}}>Completeness of vision<span>&#8594;</span></div>
         <div style={{border: "2px solid lightgrey", position:"absolute", bottom: "50%", width:"99%"}}/>
         <div style={{border: "2px solid lightgrey", position:"absolute", bottom: "50%", width:"99%", transform:"rotate(90deg)"}}/>
+
+        <div style={{position:"absolute", bottom:"1%", left:"12.5%", color:"white", backgroundColor:"darkgrey", borderRadius:"3px"}}>Niche Players</div>
+        <div style={{position:"absolute", bottom:"1%", left:"65%", color:"white", backgroundColor:"darkgrey", borderRadius:"3px"}}>Visionaries</div>
+        <div style={{position:"absolute", bottom:"94%", left:"15%", color:"white", backgroundColor:"darkgrey", borderRadius:"3px"}}>Challengers</div>
+        <div style={{position:"absolute", bottom:"94%", left:"67.5%", color:"white", backgroundColor:"darkgrey", borderRadius:"3px"}}>Leaders</div>
     </div>
   );
 }
